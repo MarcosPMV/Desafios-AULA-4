5e9c9b5611602a19952ef31f86164268fafb1be4
Marcos Martins <marcospmvplay@gmail.com>
2025-06-26 21:09:02 -0300

    m

--- 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -1,20 +1,47 @@
+let amigos = [];
+
 function adicionar() {
-let nomeAmigo = document.getElementById('nome-amigo');
-let listaAmigos = document.getElementById ('lista-amigos');
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
 
-  if (listaAmigos == '') {
-listaAmigos.textContent = nomeAmigo.value;
+    amigos.push(amigo.value);
 
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
 
-} else {
-listaAmigos.textContent = listaAmigos.textContent + ', ' + nomeAmigo.value;
+    amigo.value = '';
 }
-  
-
-}
-
-
-
 function sortear() {
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++) {
+   
+    if (i == amigos.length -1){
+       sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+    } else {
+ sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+    }
+}    
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 
 }
