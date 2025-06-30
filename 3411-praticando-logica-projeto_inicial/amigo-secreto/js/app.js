1d71b75b2e16fc304fe590d28b8b05efa9490ab3
Marcos Martins <marcospmvplay@gmail.com>
2025-06-30 20:10:58 -0300

    novonovo

--- 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -1,22 +1,36 @@
 let amigos = [];
 
 function adicionar() {
-    let amigo = document.getElementById('nome-amigo');
-    let lista = document.getElementById('lista-amigos');
 
+    let amigo = document.getElementById('nome-amigo');
+    if (amigo.value == ''){
+        alert ('Adicione um nome.');
+        return;
+    }
+    if (amigos.includes(amigo.value)) {
+    alert(`O nome ${amigo.value} já foi escolhido.`);
+    return;
+  } 
+  
+  let lista = document.getElementById('lista-amigos'); 
     amigos.push(amigo.value);
 
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
     } else {
         lista.textContent = lista.textContent + ', ' + amigo.value;
-    }
-
+    } 
     amigo.value = '';
 }
+
 function sortear() {
+    if (amigos.length <= 3){
+    alert ('O número minímo de participantes para o sorteio é 4.');
+return
+}
     embaralha(amigos);
     let sorteio = document.getElementById('lista-sorteio');
+
 
     for (let i = 0; i < amigos.length; i++) {
    
